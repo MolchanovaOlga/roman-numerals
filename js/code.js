71dec4fb7804d72af0d304fbadb3337ee420044b
MolchanovaOlga <designer.molchanova@gmail.com>
2023-12-16 22:24:30 +0200

    Added solution to task of converting an integer  to a roman numeral

--- js/code.js
+++ js/code.js
@@ -9,7 +9,7 @@ const obj = {
 }
 
 function romanToInt(s) {
-    const arrOfNums =  s.split('').map(num => obj[num]);
+    const arrOfNums =  s.split('').map(numb => obj[numb]);
     let sum = 0;
     for (let i = 0; i < arrOfNums.length; i += 1) {
         if (arrOfNums[i] < arrOfNums[i + 1]) {
@@ -21,7 +21,49 @@ function romanToInt(s) {
     return sum;
 }
 
-console.log(romanToInt("XLIV"))
+console.log(romanToInt("CCC"))
+
+function intToRoman(num) {
+    const arrOfNum = num.toString().split('').reverse();
+
+    const a = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    const b = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+    const c = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+    const d = ['M', 'MM', 'MMM'];
+
+    for (let i = 0; i < arrOfNum.length; i += 1) {
+        const arrOfRoman = [];
+        if (arrOfNum[i] !== 0) {
+            for (let j = 0; j < a.length; j += 1) {
+                if (arrOfNum[0] - 1 === j) {
+                arrOfRoman.push(a[j]);
+                }
+            }
+
+            for (let k = 0; k < b.length; k += 1) {
+                if (arrOfNum[1] - 1 === k) {
+                arrOfRoman.push(b[k]);
+                }
+            }
+
+            for (let l = 0; l < c.length; l += 1) {
+                if (arrOfNum[2] - 1 === l) {
+                arrOfRoman.push(c[l]);
+                }
+            }
+
+            for (let m = 0; m < d.length; m += 1) {
+                if (arrOfNum[3] - 1 === m) {
+                arrOfRoman.push(d[m]);
+                }
+            }
+        }
+        const str = arrOfRoman.reverse().join('');
+        return str;
+    }
+}
+console.log(intToRoman(1245))
+
 
 
 
